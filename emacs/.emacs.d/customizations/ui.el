4375e82bf4c37379424ba98b89aa6f7db231f616
Andrew Slotin <andrew.slotin@gmail.com>
2015-08-20 11:03:39 +0200

    emacs: use tango-dark theme

--- emacs/.emacs.d/customizations/ui.el
+++ emacs/.emacs.d/customizations/ui.el
@@ -27,7 +27,7 @@
 ;; for a more technical explanation.
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (add-to-list 'load-path "~/.emacs.d/themes")
-(load-theme 'zenburn t)
+(load-theme 'tango-dark t)
 
 ;; increase font size for better readability
 (set-face-attribute 'default nil :height 140)
